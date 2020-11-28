e33004d09f362067fb83168d358f1889e8bc8e8e
prerna <bhatraprerna061@gmail.com>
2020-11-28 18:29:57 +0530

    texteditor

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,36 +1,57 @@
-import React,{useState,useEffect,useRef} from 'react'
+import React,{useState,useEffect} from 'react'
 import Layout from './Layout'
 import {Link} from 'react-router-dom'
 import {isAuthenticated} from '../auth'
 import axios from 'axios'
 import uuid from 'react-uuid'
 import {BASE_URL} from "../config.js"
-import JoditEditor from "jodit-react";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 //{JSON.stringify(slots)}
 const Home=()=>{
-	const editor = useRef(null)
-	const [content, setContent] = useState('')
-	
-	const config = {
-		readonly: false // all options from https://xdsoft.net/jodit/doc/
-	}
-	
+	const [value,setValue]=useState("")
+		const handleChange=(e,editor)=>
+		{
+			//console.log(editor.getData())
+			const data=editor.getData()
+			setValue(data);
+			console.log(value)
+		}
+		const handleSubmit=()=>
+		{
+
+			console.log("form data"+value);
+		}
+
+		useEffect(() => {
+    // action on update of movies
+}, [value]);
+
 	return (
-		<div style={{"margin-top":"70px"}}>
-            <JoditEditor
+		  <div className="container">
+                <h2>Using CKEditor 5 build in React</h2>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello write your post here</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                   onChange={handleChange}
+                    onBlur={ ( event, editor ) => {
+                        //console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                       // console.log( 'Focus.', editor );
+                    } }
+                />
+                <button onClick={handleSubmit} className="btn btn-primary" style={{"float":"right"}} >Post</button>
+                { ReactHtmlParser(value) }
+            </div>
+		)
 
-            	ref={editor}
-                value={content}
-                config={config}
-		tabIndex={1} // tabIndex of textarea
-		onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-                onChange={newContent => {}}
-            />
-            <input type="submit"  className="btn btn-primary" value="POST" style={{"float":"right"}}/>  
-
-           </div>
-        );
 }
 
 export default Home
