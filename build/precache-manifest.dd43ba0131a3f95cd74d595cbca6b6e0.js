b495cc3e1e193ea2330ffac0c5ed4d0d4f0d7e11
prerna <bhatraprerna061@gmail.com>
2020-12-01 18:19:30 +0530

    buil

---
+++ build/precache-manifest.dd43ba0131a3f95cd74d595cbca6b6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1541e60f2b56ee349e5812ea789794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "275a436b56ed82df8497",
+    "url": "/static/css/main.7013c982.chunk.css"
+  },
+  {
+    "revision": "4c6e102b963582708092",
+    "url": "/static/js/2.1fdc00f3.chunk.js"
+  },
+  {
+    "revision": "db311c8aa0d781086e0eba8267f7edd1",
+    "url": "/static/js/2.1fdc00f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275a436b56ed82df8497",
+    "url": "/static/js/main.07410ff2.chunk.js"
+  },
+  {
+    "revision": "2699054429d891176495",
+    "url": "/static/js/runtime-main.a9be9282.js"
+  },
+  {
+    "revision": "71241fedd86d33706f071423006bb53a",
+    "url": "/static/media/blog.71241fed.jpeg"
+  }
+]);
